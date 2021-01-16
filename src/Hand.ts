86e6b768c2f8b3922253d297ec9203a6ff34aeef
a.maki <a.maki@sw-s.ne.jp>
2021-01-16 10:07:57 +0900

    HandクラスにplayCardメソッドの実装

--- src/Hand.ts
+++ src/Hand.ts
@@ -2,6 +2,7 @@ import Card from "./Card";
 
 export default class Hand {
   private cards: Array<Card>;
+  static NUM_OF_PLAY_CARD_AT_ONE_TIME = 1;
 
   public constructor(numOfCards: number) {
     this.cards = [...Array(numOfCards)].map((_, i) => new Card(i + 1));
@@ -9,5 +10,11 @@ export default class Hand {
 
   public getCards() : Array<Card>{
     return this.cards;
+  }
+
+  public playCard(cardNumber: number) : Card {
+    const index = this.cards.findIndex(c => c.number === cardNumber);
+    const playCards = this.cards.splice(index, Hand.NUM_OF_PLAY_CARD_AT_ONE_TIME);
+    return playCards[0];
   }
 }
--- src/__tests__/Hand.ts
+++ src/__tests__/Hand.ts
@@ -2,10 +2,19 @@ import Card from "../Card";
 import Hand from "../Hand";
 
 const NUM_OF_CARDS = 7;
+const CARD_NUMBER_OF_PLAY = 4;
 
 test('make hand', () => {
   const hand = new Hand(NUM_OF_CARDS);
   expect(NUM_OF_CARDS).toBe(hand.getCards().length);
   const cards = [...Array(NUM_OF_CARDS)].map((_, i) => new Card(i+1));
   expect(cards).toStrictEqual(hand.getCards());
+});
+
+test('play a card', () => {
+  const hand = new Hand(NUM_OF_CARDS);
+  const card = hand.playCard(CARD_NUMBER_OF_PLAY);
+  const expectingCard = new Card(CARD_NUMBER_OF_PLAY);
+  expect(expectingCard).toStrictEqual(card);
+  expect(NUM_OF_CARDS - 1).toBe(hand.getCards().length);
 });
